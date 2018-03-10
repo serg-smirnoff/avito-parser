4d2b4f8279338bac758099b40295b1dea6f1cbd4
root <root@onlysites.ru>
2018-03-10 03:07:33 +0300

    avito-parser 2.

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-echo "1";
 
+$url = "https://www.avito.ru/kaliningrad/avtomobili/skoda_octavia_2014_1400675007";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+curl_setopt($ch, CURLOPT_URL, $url);
+$res = curl_exec($ch);
+curl_close($ch);
+
+echo trim($res);
 
 ?>
